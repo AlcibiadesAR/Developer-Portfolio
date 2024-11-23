eb527b853504fb8c997339fbd805fcf20358d6d1
AlcibiadesAR <alcibiades.acosta.r@gmail.com>
2024-11-23 10:28:31 -0500

    implementacion de un hooks

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="bg-gray-900 font-quantico text-white py-4 mt-2">
       <div className="container m-auto text-center">
         <p className="text-sm text-center px-10 md:px-8">
-          Desarrollado por <a href="/" className="text-red-500">Alcibiades Acosta</a> | Copyright &copy; {new Date().getFullYear()}
+          Desarrollado por <a href="/" className="text-red-500">Alcibiades Acosta</a> | {new Date().getFullYear()}
         </p>
       </div>
     </footer>
